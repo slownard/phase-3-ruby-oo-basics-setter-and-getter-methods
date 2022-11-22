0206beeea3a1eed0dcd6b3d0d0a4f0c2dfe74d44
Bernard Legaspi <blegaspi26@gmail.com>
2022-11-22 16:27:59 -0500

    commit tree 37e2db89f16adfc0aceaed7e1dba1fa61ad8e0f3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
 
+    
+
+    #setter 
+
+    def name 
+        @name 
+    end
+
+    def name=(name)
+        @name=name 
+    end 
+
+
+    def breed
+        @breed
+    end 
+
+    def breed=(breed)
+        @breed=breed
+    end
+
 end
+
+dog.new 
+
